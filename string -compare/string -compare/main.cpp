1544e73b8a6b98ff4c6185e7a3ac5b371ab984b4
Nisha Ramprasath <115043583+nisharampra@users.noreply.github.com>
2024-01-08 23:04:34 +0800

    Add files via upload

---
+++ string -compare/string -compare/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  string -compare
+//
+//  Created by Nisha Ramprasath on 3/11/23.
+//
+
+#include <iostream>
+using namespace std;
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    string str1="hello";
+    string str2="world";
+    
+    cout<<str1.compare(str2);
+    cout<<endl;
+    return 0;
+}
